dc86529e506821d3ed878c2f7b15a1b45235718c
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-16 18:00:10 -0500

    created my database, PDO connection, and retrieved data from the database and displayed it

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;dbname=Macro-Manager;charset=utf8mb4;user=root";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from meals");
+
+$statement->execute();
+
+$meals = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($meals as $meal) {
+    echo "<li>" . $meal['brand'] . " " . $meal['food_item'] . ":" . $meal['calories']. "  calories" . "</li>";
+}
